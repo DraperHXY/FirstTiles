279071f3267294bb6bdd19ee36daac6850ace8fc
Draper_HXY <draper_hxy@163.com>
2018-11-08 17:12:23 +0800

    Interceptor 补充

---
+++ src/main/java/com/draper/web/interceptor/UserInterceptor.java
@@ -0,0 +1,43 @@
+package com.draper.web.interceptor;
+
+import com.draper.service.CookieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class UserInterceptor implements HandlerInterceptor {
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private CookieService cookieService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        if (cookieService.verifyUser(cookies)) {
+            LOGGER.warn("验证成功");
+        } else {
+            LOGGER.warn("验证失败");
+            modelAndView.setViewName("loginView");
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+
+}
